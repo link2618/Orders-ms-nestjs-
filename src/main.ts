b80c1b80cfdf4532b223735b9a42bcc7a9e73ed2
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-18 16:35:40 -0500

    Crud orders

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
     const logger = new Logger('OrdersMS-Main');
-    const app = await NestFactory.create(AppModule);
 
-    await app.listen(envs.port);
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        AppModule,
+        {
+            transport: Transport.TCP,
+            options: {
+                port: envs.port,
+            },
+        },
+    );
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen();
 
     logger.log(`Microservice running on port ${envs.port}`);
 }
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, ParseUUIDPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { OrdersService } from './orders.service';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { CreateOrderDto, ChangeOrderStatusDto } from './dto';
+
+@Controller()
+export class OrdersController {
+    constructor(private readonly ordersService: OrdersService) {}
+
+    @MessagePattern('createOrder')
+    create(@Payload() createOrderDto: CreateOrderDto) {
+        return this.ordersService.create(createOrderDto);
+    }
+
+    @MessagePattern('findAllOrders')
+    findAll(@Payload() orderPaginationDto: OrderPaginationDto) {
+        return this.ordersService.findAll(orderPaginationDto);
+    }
+
+    @MessagePattern('findOneOrder')
+    findOne(@Payload('id', ParseUUIDPipe) id: string) {
+        return this.ordersService.findOne(id);
+    }
+
+    @MessagePattern('changeOrderStatus')
+    changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto) {
+        console.log(changeOrderStatusDto);
+        return this.ordersService.changeStatus(changeOrderStatusDto);
+    }
+}

394f8ebed73b7a025be7ae340ded120ebf8cdc0f
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-18 14:54:56 -0500

    Initial config

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+    const logger = new Logger('OrdersMS-Main');
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(envs.port);
+
+    logger.log(`Microservice running on port ${envs.port}`);
+}
+bootstrap();
